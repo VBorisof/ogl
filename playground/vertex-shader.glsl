dd68f19a6854d6b4134dc912404715a8971e5f42
pteberf <fatal1978@protonmail.com>
2020-05-01 21:20:02 +0300

    Update textured cube

--- playground/vertex-shader.glsl
+++ playground/vertex-shader.glsl
@@ -7,7 +7,9 @@ out vec2 UV;
 uniform mat4 MVP;
 
 void main() {
-    gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+    vec3 pos = vertexPosition_modelspace;
+
+    gl_Position = MVP * vec4(pos, 1);
 
     UV = vertexUV;
 }
